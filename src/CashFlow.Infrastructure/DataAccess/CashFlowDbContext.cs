24212b99eec0e9e57016a9c3d748ab11c377658b
Coimbra-Rafael <rcoimbra2022@outlook.com>
2025-04-30 15:53:20 -0300

    feat: database intruduction

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    public CashFlowDbContext(DbContextOptions<CashFlowDbContext> options) : base(options) {}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cash_flow;Uid=rafael;Pwd=1qazzaq!;";
+        var serverVersion = new MySqlServerVersion(new Version("8.0.39"));
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+
+    }
+
+}
